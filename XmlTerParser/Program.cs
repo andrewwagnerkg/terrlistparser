5336c592e79a4778e8dbf30625c675d1feed4db3
andrewwagnerkg <38209339+andrewwagnerkg@users.noreply.github.com>
2020-07-03 22:27:32 +0600

    Update Program.cs
    Adding try catch constructions

--- XmlTerParser/Program.cs
+++ XmlTerParser/Program.cs
@@ -7,30 +7,54 @@ namespace XmlTerParser
         static void Main(string[] args)
         {
             Console.WriteLine("Start downloading...");
-            IXmlDownloader xmlDownloader = new ConsolidateDownloader();
-            xmlDownloader.Download("https://scsanctions.un.org/resources/xml/en/consolidated.xml");
-            xmlDownloader = new ConsolidateKGXmlDownloader();
-            xmlDownloader.Download("https://fiu.gov.kg/uploads/5ef473e90f77c.xml");
+            IXmlDownloader xmlDownloader;
+            try
+            {
+                xmlDownloader = new ConsolidateDownloader();
+                xmlDownloader.Download("https://scsanctions.un.org/resources/xml/en/consolidated.xml");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+            try
+            {
+                xmlDownloader = new ConsolidateKGXmlDownloader();
+                xmlDownloader.Download("https://fiu.gov.kg/uploads/5ef473e90f77c.xml");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+
             Console.WriteLine("Downloading complete");
 
-            IApplySql applygeneric = new ApplySqlToFile("genericsql.sql");
-            IApplySql applyterminalv3 = new ApplySqlToFile("terminalv3sql.sql");
+            try
+            {
+                IApplySql applygeneric = new ApplySqlToFile("genericsql.sql");
+                IApplySql applyterminalv3 = new ApplySqlToFile("terminalv3sql.sql");
 
-            IParser parser = new ConsolidatedKgXMLParser();
-            IParser worldparser = new ConsolidatedWorldXMLParser();
-            
-            IBuilder sqlbuilder=new GenericSqlBuilder();
-            Console.WriteLine(applygeneric.Apply(parser.Parse("consolidatedKG.xml",sqlbuilder)));
+                IParser parser = new ConsolidatedKgXMLParser();
+                IParser worldparser = new ConsolidatedWorldXMLParser();
 
-            sqlbuilder = new GenericSqlBuilder();
-            Console.WriteLine(applygeneric.Apply(worldparser.Parse("consolidated.xml", sqlbuilder)));
+                IBuilder sqlbuilder = new GenericSqlBuilder();
+                Console.WriteLine(applygeneric.Apply(parser.Parse("consolidatedKG.xml", sqlbuilder)));
 
-            sqlbuilder = new TerminalV3SqlBuilder();
-            Console.WriteLine(applyterminalv3.Apply(parser.Parse("consolidatedKG.xml",sqlbuilder)));
+                sqlbuilder = new GenericSqlBuilder();
+                Console.WriteLine(applygeneric.Apply(worldparser.Parse("consolidated.xml", sqlbuilder)));
 
-            sqlbuilder = new TerminalV3SqlBuilder();
-            Console.WriteLine(applyterminalv3.Apply(worldparser.Parse("consolidated.xml", sqlbuilder)));
+                sqlbuilder = new TerminalV3SqlBuilder();
+                Console.WriteLine(applyterminalv3.Apply(parser.Parse("consolidatedKG.xml", sqlbuilder)));
 
+                sqlbuilder = new TerminalV3SqlBuilder();
+                Console.WriteLine(applyterminalv3.Apply(worldparser.Parse("consolidated.xml", sqlbuilder)));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
             Console.WriteLine("finish");
         }
     }
